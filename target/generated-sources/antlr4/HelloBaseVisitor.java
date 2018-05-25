106e7402ce6093ba2e98f75e4199fcd955f7159c
rlaehgns5399 <rlaehgns5398@gmail.com>
2018-05-25 12:25:09 +0900

    Fix
    Fixed Grammer Error and Add AST GUI Tree

--- target/generated-sources/antlr4/HelloBaseVisitor.java
+++ target/generated-sources/antlr4/HelloBaseVisitor.java
@@ -86,9 +86,7 @@ public class HelloBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements 
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitExpression_array(HelloParser.Expression_arrayContext ctx) { 
-		System.out.println(ctx.getText());
-		return visitChildren(ctx); }
+	@Override public T visitExpression_array(HelloParser.Expression_arrayContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
--- target/generated-sources/antlr4/main.java
+++ target/generated-sources/antlr4/main.java
@@ -1,21 +1,41 @@
+import java.awt.Toolkit;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import org.antlr.v4.gui.TreeViewer;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.ParseTree;
-
 public class main {
-
+	public static ParseTree makeASTtree(HelloParser parser){
+		ParseTree tree = parser.program();
+	    JFrame frame = new JFrame("Antlr AST");
+	    JPanel panel = new JPanel();
+	    TreeViewer viewr = new TreeViewer(Arrays.asList(
+	                parser.getRuleNames()),tree);
+	    viewr.setScale(1.5);
+	    panel.add(viewr);
+	    frame.add(panel);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setSize(Toolkit.getDefaultToolkit().getScreenSize().width,Toolkit.getDefaultToolkit().getScreenSize().height);
+	    frame.setVisible(true);
+	    
+	    return tree;
+	    
+	}
 	public static void main(String[] args) {
 		try{
 		HelloLexer lexer = new HelloLexer( new ANTLRFileStream("test.c"));
 		CommonTokenStream tokens = new CommonTokenStream( lexer );
 		HelloParser parser = new HelloParser( tokens );
-		ParseTree tree = parser.program();
-		
-		new HelloBaseVisitor().visit(tree);
+		ParseTree tree = makeASTtree(parser);
+	        
+		//new HelloBaseVisitor().visit(tree);
 		
 		/*
 		MiniCAstVisitor visitor = new MiniCAstVisitor();
